735601096a16c3ba2f064aacf46aa0fc22f96b35
namrata <namrata@namrata-Inspiron-3542>
2015-11-29 15:05:59 +0530

    final

--- smartpayservice/src/main/java/com/novadit/smartpayservice/UserController.java
+++ smartpayservice/src/main/java/com/novadit/smartpayservice/UserController.java
@@ -1,29 +1,13 @@
 package com.novadit.smartpayservice;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.novadit.smartpayservice.Name;
 
 @RestController
 public class UserController {
 
     @RequestMapping(value = "/users")
 	public User getUser(){
-    	
-    	User u1 = new User("1", new Name("sd","dfa"));
-		return u1;
-	}
-    
-    @RequestMapping(value = "/contact", method=RequestMethod.POST)
-	public User saveContact(@RequestBody Contact contact){
-    	
-    	System.out.println(contact.toString());
     	
     	User u1 = new User("1", new Name("sd","dfa"));
 		return u1;
